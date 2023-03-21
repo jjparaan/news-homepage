f0a32093e8b5ebd4d464958bc4e5ced61e6723fa
jhuancho <84026547+gitjhuanjulio@users.noreply.github.com>
2023-03-21 22:40:43 +0800

    Updated

--- src/items.js
+++ src/items.js
@@ -20,4 +20,25 @@ export const items = [
 		title: "The Growth of Gaming",
 		details: "How the pandemic has sparked fresh opportunities",
 	},
+	{
+		id: 4,
+		img: "/assets/images/image-retro-pcs.jpg",
+		serial: `04`,
+		title: "Reviving Retro PCs",
+		details: "What happens when old PCs are given modern upgrades?",
+	},
+	{
+		id: 5,
+		img: "/assets/images/image-top-laptops.jpg",
+		serial: `05`,
+		title: "Top 10 Laptops of 2022",
+		details: "Our best picks for various needs and budgets",
+	},
+	{
+		id: 6,
+		img: "/assets/images/image-gaming-growth.jpg",
+		serial: `06`,
+		title: "The Growth of Gaming",
+		details: "How the pandemic has sparked fresh opportunities",
+	},
 ];
--- src/news.js
+++ src/news.js
@@ -16,4 +16,21 @@ export const news = [
 		details:
 			"Private funding by VC firms is down 50% YOY. We take a look at what that means.",
 	},
+	{
+		id: 4,
+		title: "Hydrogen VS Electric Cars",
+		details: "Will hydrogen-fueled cars ever catch up to EVs?",
+	},
+	{
+		id: 5,
+		title: "The Downsides of AI Artistry",
+		details:
+			"What are the possible adverse effects of on-demand AI image generation?",
+	},
+	{
+		id: 6,
+		title: "Is VC Funding Drying Up?",
+		details:
+			"Private funding by VC firms is down 50% YOY. We take a look at what that means.",
+	},
 ];
